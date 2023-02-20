e0e4220cab657a5520f22a2353b0a6f6ed41f520
opilane <opilane@HP-CZC2349HTB>
2023-02-20 14:33:37 +0200

    Add project files.

---
+++ Sonastik_Rogovski.py
@@ -0,0 +1,8 @@
+﻿from functions import *
+
+while True:
+    choice = int(input("1 - est->rus\n2 - rus->est\n"))
+    if choice == 1:
+        print()
+    elif choice == 2:
+        print()
---
+++ functions.py
@@ -0,0 +1,6 @@
+﻿from os import *
+from gtts import gTTS
+from random import *
+
+def estrus_trans():
+    print()
