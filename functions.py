d45f6a1af8cfb2c59cb2052f93aad8e0dc2fc460
tirfall <tirfall@DESKTOP-MEDCFF5>
2023-02-22 12:36:28 +0200

    1.2

--- functions.py
+++ functions.py
@@ -4,3 +4,13 @@ from random import *
 
 def estrus_trans():
     print()
+
+def loe_failist(f):
+    fail=open(f,'r',encoding="utf-8-sig")
+    mas=[] 
+    for rida in fail:
+        mas.append(rida.strip())
+    fail.close()
+    return mas
+
+
