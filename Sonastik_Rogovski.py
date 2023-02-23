5bce42537ee1c43f88c8a14c448b558a1260c616
opilane <opilane@HP-CZC2349HTB>
2023-02-23 11:25:47 +0200

    1.3

--- Sonastik_Rogovski.py
+++ Sonastik_Rogovski.py
@@ -1,22 +1,29 @@
-﻿from functions import *
+from functions import *
 
-est=loe_failist("est.txt")
-rus=loe_failist("rus.txt")
-eng=loe_failist("eng.txt")
-
-
+est:list=loe_failist("est.txt")
+rus:list=loe_failist("rus.txt")
+eng:list=loe_failist("eng.txt")
 
 while True:
-    choice = int(input("1 - gtts\n2 - lisa sõna\n3 - test\n4 - Parandada viga\n5 - est->rus\n6 - rus->est\n"))
+    choice = int(input("0 - exit\n1 - gtts\n2 - lisa sõna\n3 - test\n4 - Parandada viga\n5 - est->rus->eng\n6 - salvestamine\n"))
     if choice == 1:
-        print()
+        Heli(rus,est,eng)
+        print(rus,est,eng)
+    elif choice == 0:
+        break
     elif choice == 2:
-        print()
+        uus_sona(rus,est,eng)
     elif choice == 3:
         print()
     elif choice == 4:
         print()
     elif choice == 5:
-        print()
+        estrus_trans(rus,est,eng)
     elif choice == 6:
-        print()
+        salvestamine("est.txt",est)
+        salvestamine("rus.txt",rus)
+        salvestamine("eng.txt",eng)
+    elif choice == 7:
+        est:list=loe_failist("est.txt")
+        rus:list=loe_failist("rus.txt")
+        eng:list=loe_failist("eng.txt")
--- functions.py
+++ functions.py
@@ -1,16 +1,85 @@
-﻿from os import *
+from os import *
 from gtts import gTTS
 from random import *
+import io
 
-def estrus_trans():
-    print()
+def estrus_trans(rus_list,est_list,eng_list):
+    slovo=input('sisesta sõna: ')
+    slovo = slovo.title()
+    if slovo in rus_list:
+        ind=rus_list.index(slovo)
+        print(f"{slovo} - {est_list[ind]} - {eng_list[ind]}")
+    elif slovo in est_list:
+        ind=est_list.index(slovo)
+        print(f"{slovo} - {rus_list[ind]} - {eng_list[ind]}")
+    elif slovo in eng_list:
+        ind=eng_list.index(slovo)
+        print(f"{slovo} - {rus_list[ind]} - {est_list[ind]}")
+    else:
+        print(f"{slovo.upper()} ei ole sõnastikus")
+        v=input("kas te soovite lisa sõna?")
+        #v = v.title
+        if v.lower()=="jah":uus_sona()
+        else:
+            pass
+
+def uus_sona(rus_list,est_list,eng_list):
+    slovo_est=input('sisesta sõna eesti: ')
+    slovo_est=slovo_est.title()
+    est_list.append(slovo_est)
+    slovo_rus=input('sisesta sõna vene: ')
+    slovo_rus=slovo_rus.title()
+    rus_list.append(slovo_rus)
+    slovo_eng=input('sisesta sõna inglise: ')
+    slovo_eng=slovo_eng.title()
+    eng_list.append(slovo_eng)
+    
 
 def loe_failist(f):
-    fail=open(f,'r',encoding="utf-8-sig")
+    fail=io.open(f, "r", encoding="utf-8-sig")
     mas=[] 
     for rida in fail:
         mas.append(rida.strip())
     fail.close()
     return mas
+
+def salvestamine(fail,jarjend):
+    f = io.open(fail, "w", encoding="utf-8-sig")
+    for element in jarjend:
+     f.write(element)
+     f.write('\n')
+    f.close()
+
+def Heli(rus_list,est_list,eng_list):
+    """
+    Helime failis
+    """
+    slovo=input('sisesta sõna: ')
+    slovo = slovo.title()
+    if slovo in rus_list:
+        ind=rus_list.index(slovo)
+        print(f"{slovo} - {est_list[ind]} - {eng_list[ind]}")
+        text= slovo
+        keel="ru"
+        gTTS(text=text, lang=keel, slow=False).save("heli.mp3")
+        system("heli.mp3")
+        text= est_list[ind]
+        keel="et"
+        gTTS(text=text, lang=keel, slow=False).save("heli.mp3")
+        system("heli.mp3")
+        text= eng_list[ind]
+        keel="en"
+        gTTS(text=text, lang=keel, slow=False).save("heli.mp3")
+        system("heli.mp3")
+    elif slovo in est_list:
+        ind=est_list.index(slovo)
+        print(f"{slovo} - {rus_list[ind]} - {eng_list[ind]}")
+        text= slovo,rus_list[ind],eng_list[ind]
+    elif slovo in eng_list:
+        ind=eng_list.index(slovo)
+        print(f"{slovo} - {rus_list[ind]} - {est_list[ind]}")
+        text= slovo,rus_list[ind],est_list[ind]
+    #gTTS(text=text, lang=keel, slow=False).save("heli.mp3")
+    system("heli.mp3")
 
 
