5db1783c6fdbb9e45cfa766b4b95b673c82e013d
opilane <opilane@HP-CZC2349HTB>
2023-03-08 15:07:16 +0200

    2.0

--- Sonastik_Rogovski.py
+++ Sonastik_Rogovski.py
@@ -1,12 +1,14 @@
 from functions import *
 
+password=8484
+
 est:list=loe_failist("est.txt")
 rus:list=loe_failist("rus.txt")
 eng:list=loe_failist("eng.txt")
 ind = 0
 while True:
     try:
-        choice = int(input("0 - exit\n1 - gtts\n2 - lisa sõna\n3 - test\n4 - Parandada viga\n5 - est->rus->eng\n6 - salvestamine\n7 - loe esimene fail\n8 - Vaata sõnastik\n"))
+        choice = int(input("0 - exit\n1 - gtts\n2 - lisa sõna\n3 - test\n4 - Parandada viga\n5 - est->rus->eng\n6 - salvestamine\n7 - loe esimene fail\n8 - Vaata sõnastik\n9 - adminipanel\n"))
         if choice == 1:
             Heli(rus,est,eng)
         elif choice == 0:
@@ -37,6 +39,14 @@ while True:
             eng:list=loe_failist("eng.txt")
         elif choice == 8:
             print(rus,est,eng)
+        elif choice == 9:
+            anspass=int(input("Sisesta password"))
+            if anspass == password:
+                adminchoice = int(input("1 - loe failid reservist\n"))
+                if adminchoice == 1:
+                    est:list=loe_failist("reserve/est.txt")
+                    rus:list=loe_failist("reserve/rus.txt")
+                    eng:list=loe_failist("reserve/eng.txt")
     except:
         ValueError
         print("Vale andmetüüp")
